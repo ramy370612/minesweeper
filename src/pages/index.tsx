433bbfea1256f95677ad2423f860587115f1436d
ramy370612 <miraifuture37@gmail.com>
2024-05-24 22:24:00 +0900

    bomb10

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,18 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
   const [userInputs, setUserInputs] = useState([
+    [1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  const [board, setboard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -27,26 +39,74 @@ const Home = () => {
   ]);
   // const board = [...Array(9)].map((_, y) => [...Array(9)].map((_, x) => ((y + x + 1) % 13) - 1));
   // const board: number[][] = [];
+  const bombset = (x: number, y: number, bombMap: number[][]) => {
+    const bombPosition: number[][] = [];
+    while (bombPosition.length < 10) {
+      const bombx = Math.floor(Math.random() * 9);
+      const bomby = Math.floor(Math.random() * 9);
+      console.log(bomby, bombx);
+      if (x === bombx && y === bomby) {
+        continue;
+      }
+      const double = [0];
+      for (const i of bombPosition) {
+        if (i[0] === bomby && i[1] === bombx) {
+          double[0]++;
+          break;
+        }
+      }
+      if (double[0] === 1) continue;
 
+      bombPosition.push([bomby, bombx]);
+    }
+    for (const s of bombPosition) {
+      bombMap[s[1]][s[0]] = 11;
+    }
+    return bombMap;
+  };
   console.log(samplePos);
   console.log(styles.box1);
-  // const clickHandler = (x, y) => {
+  const clickHandler = (x: number, y: number) => {
+    const newbomMap = structuredClone(bombMap);
+    setBombMap(bombset(x, y, newbomMap));
+    console.log(x, y);
+  };
   //   console.log(`Cell clicked at x: ${x}, y: ${y}`);
   //   // ここにセルをクリックした際の処理を追加
   // };
   return (
     <div className={styles.container}>
       <div className={styles.bigboardStyle}>
-        <div className={styles.boardStyle}>
+        <div className={styles.pointStyle}>
           <div className={styles.nicoStyle}>
+            <div
+              className={styles.sampleStyle}
+              style={{ backgroundPosition: `${samplePos * -30}px 0px` }}
+            />
             {/* <button onClick={() => setsamplePos((p) => (p + 1) % 14)}>sample</button> */}
           </div>
         </div>
 
         <div className={styles.gameboardStyle}>
-          {userInputs.map((row, y) =>
-            row.map((number, x) => (
-              <div className={styles.stoneStyle} key={`${x}-${y}`}>
+          {bombMap.map((row, y) =>
+            row.map((cell, x) => (
+              <div
+                className={styles.stoneStyle}
+                onClick={() => clickHandler(x, y)}
+                key={`${x}-${y}`}
+                style={{
+                  borderColor: bombMap[y][x] >= 0 ? '#909090' : '#fff #909090 #909090 #fff',
+                }}
+              >
+                <div
+                  className={styles.sampleStyle}
+                  style={{
+                    backgroundPosition: `${(bombMap[y][x] - 1) * -30}px 0px`,
+                  }}
+
+                  // onClick={() => clickHandler(y, x)}
+                />
+
                 {/* <div
                   className={styles.sampleStyle}
                   style={{ backgroundPosition: `${samplePos * -30}px 0px` }}
